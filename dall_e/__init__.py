b1473d37c0d525ad6306652e178662505a385f06
enhuiz <niuzhe.nz@outlook.com>
2023-01-09 16:35:39 +0800

    Update

--- dall_e/__init__.py
+++ dall_e/__init__.py
@@ -1,18 +1,32 @@
-import io, requests
+import io
+
+import requests
 import torch
 import torch.nn as nn
 
-from dall_e.encoder import Encoder
 from dall_e.decoder import Decoder
-from dall_e.utils   import map_pixels, unmap_pixels
+from dall_e.encoder import Encoder
+from dall_e.utils import map_pixels, unmap_pixels
+
+
+def _initialize(model):
+    if model.__class__.__name__ == "Encoder":
+        ret = Encoder()
+    elif model.__class__.__name__ == "Decoder":
+        ret = Decoder()
+    else:
+        raise NotImplementedError
+    ret.load_state_dict(model.state_dict())
+    return ret
+
 
 def load_model(path: str, device: torch.device = None) -> nn.Module:
-    if path.startswith('http://') or path.startswith('https://'):
+    if path.startswith("http://") or path.startswith("https://"):
         resp = requests.get(path)
         resp.raise_for_status()
-            
+
         with io.BytesIO(resp.content) as buf:
-            return torch.load(buf, map_location=device)
+            return _initialize(torch.load(buf, map_location=device))
     else:
-        with open(path, 'rb') as f:
-            return torch.load(f, map_location=device)
+        with open(path, "rb") as f:
+            return _initialize(torch.load(f, map_location=device))
